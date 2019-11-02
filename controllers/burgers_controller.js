9d38454dae2108ab4680151a82b2d24beaae353a
Madeleine Prak <madeleineprak@gmail.com>
2019-11-01 18:08:49 -0700

    Troubleshoot heroku

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -3,12 +3,7 @@ var burger = require("../models/burger.js");
 
 var router = express.Router();
 
-
 router.get("/", function(req, res) {
-    res.redirect("/burgers");
-});
-
-router.get("/burgers", function(req, res) {
     burger.displayAll(function(data) {
         var burgersObject = {
             burgers: data
--- server.js
+++ server.js
@@ -6,11 +6,15 @@ var PORT = process.env.PORT || 8080;
 var app = express();
 
 // Serve static content for the app from the "public" directory in the application directory.
-// app.use(express.static("public"));
+app.use(express.static("public"));
 
 // Parse application body as JSON
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+// Import routes and give the server access to them.
+var routes = require("./controllers/burgers_controller");
+app.use(routes);
 
 // Set Handlebars.
 var exphbs = require("express-handlebars");
@@ -18,10 +22,7 @@ var exphbs = require("express-handlebars");
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-// Import routes and give the server access to them.
-var routes = require("./controllers/burgers_controller");
 
-app.use(routes);
 
 // Start our server so that it can begin listening to client requests.
 app.listen(PORT, function() {
