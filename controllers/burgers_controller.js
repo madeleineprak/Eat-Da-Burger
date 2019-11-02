219a14b7530671ed61b29e97cb0cebdafe75d404
Madeleine Prak <madeleineprak@gmail.com>
2019-11-01 17:27:53 -0700

    Fix delete button

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -2,13 +2,6 @@ var express = require("express");
 var burger = require("../models/burger.js");
 
 var router = express.Router();
-var lunches = [
-    {
-      lunch: "Beet & Goat Cheese Salad with minestrone soup."
-    }, {
-      lunch: "Pizza, two double veggie burgers, fries with a Big Gulp"
-    }
-  ];
 
 router.get("/", function(req, res) {
     burger.displayAll(function(data) {
@@ -50,7 +43,7 @@ router.delete("/api/burgers/:id", function(req, res) {
     burger.deleteBurger([
         req.params.id
     ], function(result) {
-        if (result.changedRows == 0) {
+        if (result.affectedRows == 0) {
             // If no rows were changed, then the ID must not exist, so 404
             return res.status(404).end();
         } else {
